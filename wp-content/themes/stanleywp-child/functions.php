edf2a2640624440050cd0a05fad6f21d227aa9d3
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-03-25 11:42:01 -0500

    Started BH-1

--- wp-content/themes/stanleywp-child/functions.php
+++ wp-content/themes/stanleywp-child/functions.php
@@ -4,8 +4,31 @@ function theme_enqueue_styles() {
     $parent_style = 'parent-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ));
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 
+/**
+ * WordPress Widgets start right here.
+ */
+function gents_widgets_init_child() {
+
+    register_sidebar(array(
+        'name' => __('Top Header', 'gents'),
+        'description' => __('header.php', 'gents'),
+        'id' => 'top-header',
+        'before_title' => '<div class="header-title"><h4>',
+        'after_title' => '</h4></div>',
+        'before_widget' => '<div id="%1$s" class="widget-wrapper %2$s">',
+        'after_widget' => '</div>'
+    ));
+}
+
+add_action('widgets_init', 'gents_widgets_init_child');
+
+
+function register_menu_top_header(){
+    register_nav_menu( 'top-header', 'Top Header' );
+}
+
+add_action( 'init', 'register_menu_top_header' );
