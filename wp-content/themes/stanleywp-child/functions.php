af09355f343a0ebed2a2258604e0698bc1daee59
Jonathan Caiaffa <jonathan.caiaffa@akendos.com>
2016-08-15 12:30:50 -0500

    Fix csutom field save

--- wp-content/themes/stanleywp-child/functions.php
+++ wp-content/themes/stanleywp-child/functions.php
@@ -99,6 +99,7 @@ function my_init_function()
 
 function my_custom_init() {
   remove_post_type_support( 'post', 'custom-fields' );
+  remove_post_type_support('page', 'custom-fields');
 }
 add_action( 'init', 'my_custom_init' );
 
